5c217ec48494a515aa231b5117243c26acd62507
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-01 13:48:54 -0700

    model for list items and association migrations

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,27 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
 
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text='the first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'the first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'item the second')
 
 class HomePageTest(TestCase):
 
