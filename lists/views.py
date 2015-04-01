d991730ebe52fba9fce5c7d227a078650ba22229
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-01 13:05:00 -0700

    using a table, part 1

--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,11 @@ from django.http import HttpResponse
 #home_page=None
 
 def home_page(request):
-    if request.method=='POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html')
+   # if request.method=='POST':
+   #     return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
     
 #return HttpResponse('<html><title>To-Do lists</title></html>')
 
