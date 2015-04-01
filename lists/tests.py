6b17258512160a4aa6b8f86976a94032ccaae273
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-01 09:26:26 -0700

    dummy url mapping, no views yet

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,14 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+    def test_root_url_is_home_page(self):
+         found=resolve('/')
+         self.assertEqual(found.func, home_page)
+
+#class SmokeTest(TestCase):
+#    def test_bad_math(self):
+#        self.assertEqual(1+1,3)
 
 
