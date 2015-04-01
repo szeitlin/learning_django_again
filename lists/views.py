c15a3408680150602d235b8095548189610ef11c
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-01 15:37:47 -0700

    always redirect after a POST

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 #from django.http import HttpResponse
 
 from lists.models import Item
@@ -6,15 +6,16 @@ from lists.models import Item
 #home_page=None
 
 def home_page(request):
-   # if request.method=='POST':
-   #     return HttpResponse(request.POST['item_text'])
-   item = Item()
-   item.text=request.POST.get('item_text', '')
-   item.save()
+   if request.method=='POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-   return render(request, 'home.html', {
-        'new_item_text': item.text,
-    })
+   #     return HttpResponse(request.POST['item_text'])
+   #item = Item()
+   #item.text=request.POST.get('item_text', '')
+   #item.save()
+
+   return render(request, 'home.html')
     
 #return HttpResponse('<html><title>To-Do lists</title></html>')
 
