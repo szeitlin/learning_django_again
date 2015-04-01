e3a092824da4809fb38b196138fd787876ba25ba
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-01 14:20:30 -0700

    using item model

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
-# Create your views here.
+from lists.models import Item
+
 #home_page=None
 
 def home_page(request):
    # if request.method=='POST':
    #     return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+   item = Item()
+   item.text=request.POST.get('item_text', '')
+   item.save()
+
+   return render(request, 'home.html', {
+        'new_item_text': item.text,
     })
     
 #return HttpResponse('<html><title>To-Do lists</title></html>')
