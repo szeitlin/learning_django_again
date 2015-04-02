962887c555f00163dd072f1d83d3e4106000fad9
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-02 11:06:09 -0700

    added second user, Francis, to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -21,7 +21,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_later(self):
-        """ A user visits the site.
+        """ Two users visit the site. Edith first, then Francis. 
         Takes advantage of the LiveServerTestCase module"""
         
         self.browser.get(self.live_server_url)
@@ -44,9 +44,10 @@ class NewVisitorTest(LiveServerTestCase):
         #type into a text box
         inputbox.send_keys('Buy peacock feathers')
 
-
         #check that it appears on the site
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_table('1: Buy peacock feathers')
 
         table=self.browser.find_element_by_id('id_list_table')
@@ -60,9 +61,36 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
 
-        #check that both items appear (refactored) 
+        #check that both items appear in Edith's list (refactored) 
         self.check_for_row_in_table('1: Buy peacock feathers')
         self.check_for_row_in_table('2: use peacock feathers to make bait for fly fishing')
+
+        #now a new user, Francis, comes along. 
+
+        ## using a new browser session to make sure Edith's list doesn't cross-contaminate
+        self.browser.quit()
+        self.browser=webdriver.Firefox()
+
+        #Francis visits the page. Make sure Edith's stuff is gone. 
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make bait for fly', page_text)
+
+        #Francis starts his own list. His list is boring. 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #Francis gets his own url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #check again for overlap with Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
     #check that there's now a unique URL
         self.fail('Finish the test!')
@@ -71,7 +99,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         """ If there are no exceptions, close the browser windows when you're done."""
-        browser.quit()
+        self.browser.quit()
     
    
 
