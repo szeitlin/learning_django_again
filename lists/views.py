1a224fdca3a54b36132af23c95fd9768b3baa026
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-03 10:24:44 -0700

    refactored urls to isolate app-specific from rest of the site

--- lists/views.py
+++ lists/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import redirect, render
-#from django.http import HttpResponse
 
 from lists.models import Item, List
 
-#home_page=None
 
 def home_page(request):
-   # if request.method=='POST':
-   #      Item.objects.create(text=request.POST['item_text'])
-   #      return redirect('/lists/the-only-list-in-the-world/')
-
-   #items = Item.objects.all()
-   #return render(request, 'home.html') #, {'items': items})
-
-
-   #     return HttpResponse(request.POST['item_text'])
-   #item = Item()
-   #item.text=request.POST.get('item_text', '')
-   #item.save()
 
    return render(request, 'home.html')
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    #items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'list':list_})
 
 def new_list(request):
@@ -31,7 +16,6 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
 
-#return HttpResponse('<html><title>To-Do lists</title></html>')
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
